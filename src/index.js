84cb3842ccb1fbb2f5aca661f2519550743e80ab
cabraham1 <christopherabraham8@gmail.com>
2023-03-16 17:37:27 +0100

    Fix likes span section

--- src/index.js
+++ src/index.js
@@ -3,9 +3,5 @@ import './style.css';
 import display from './modules/displayList.js';
 import likes from './modules/likesCounter.js';
 
-<<<<<<< HEAD
 display();
 likes();
-=======
-display();
->>>>>>> cd4fa96a45742d6bfec0a590237bb1e536d2369b

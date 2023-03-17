3363ce23f636090b818d9430856029d9601b0fb9
cabraham1 <christopherabraham8@gmail.com>
2023-03-17 01:43:22 +0100

    Implement modal popup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import './index.html';
 import './style.css';
 import display from './modules/displayList.js';
+import Meal from './modules/meal-Class.js';
 import menuCounter from './modules/itemCounter.js';
 import { likes, displayLikes } from './modules/likesCounter.js';
+import './assets/close.svg';
 
 display();
 likes();
 displayLikes();
 menuCounter();
+const niceMeal = new Meal();
+niceMeal.getMeal();

a0600e1cddaeae6b8698076e320c710b6c8bcf74
cabraham1 <christopherabraham8@gmail.com>
2023-03-17 04:16:44 +0100

    update item count using async and await

--- src/modules/itemCounter.js
+++ src/modules/itemCounter.js
@@ -2,13 +2,11 @@ const API_URL = 'https://www.themealdb.com/api/json/v1/1/filter.php?a=British';
 const itemCounter = document.querySelector('.item-counter');
 
 const menuCounter = async () => {
-  await fetch(API_URL)
-    .then((response) => response.json())
-    .then((data) => {
-      const meal = data.meals;
-      const count = meal.length;
-      itemCounter.innerHTML = `(${count})`;
-    });
+  const response = await fetch(API_URL);
+  const data = await response.json();
+  const meal = data.meals;
+  const count = meal.length;
+  itemCounter.innerHTML = `(${count})`;
 };
 
 export default menuCounter;
